2636c140c2df3033e2c4e2991d891ee7714ea0cd
Espoir <espoiragbessi767@gmail.com>
2024-05-31 16:51:00 +0100

    Starting with the Django Rest Framework

--- py_client/basic.py
+++ py_client/basic.py
@@ -3,6 +3,10 @@ import requests
 endpoint = "http://localhost:8000/api"
 
 get_response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello world !"})
+# HTTP Request --> HTML
+# REST API HTTP Request --> JSON
 # print(get_response.text)
-print(get_response.json()["message"])
+# print(get_response.json())
+# print(get_response.headers)
+print(get_response.json())
 print(get_response.status_code)
